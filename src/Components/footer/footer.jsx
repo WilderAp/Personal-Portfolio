322a7ebf22588bd0cdc523c0b7216a07dd5b6e8a
Wilder Ap <wilderasprilla321@gmail.com>
2023-11-01 02:32:25 -0500

    Cambio en Contacts

--- src/Components/footer/footer.jsx
+++ src/Components/footer/footer.jsx
@@ -1,10 +1,14 @@
 import {BiLogoGmail} from "react-icons/bi/index.esm";
 import {AiFillLinkedin} from "react-icons/ai/index.esm";
+import "./footer.css"
 
 const Footer = () => {
     return(
+        <div className="contacts-footer">
+        <div className='works-main' style={{ fontSize: "24px", marginBottom: "20px" }}>
+                <span><span style={{ color: '#168aad' }}></span>Contacts: <span style={{ color: "#8338ec" }}>-----------------------------------------------------------------------------------</span></span>
+            </div>
         <footer className="footer" id="footer" style={{display: "flex", justifyContent: "center", margin: "0px",}}>
-            
             <figure style={{margin: "50px"}}>
                 <a href="https://www.linkedin.com/in/wilder-asprilla-pinilla-a5749220b/" rel="noreferrer" target=
                 "_blank" style={{textDecoration: "none", color: "inherit"}}>
@@ -18,6 +22,7 @@ const Footer = () => {
             </figure>
           
         </footer>
+        </div>
     )
 }
 
